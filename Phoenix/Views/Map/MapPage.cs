1855ec143200fa8a73c598feb587816ff0845581
Giusepe Casagrande <giusepe@gmail.com>
2014-08-29 15:38:31 -0300

    Removido o SVG

--- Phoenix/Views/Map/MapPage.cs
+++ Phoenix/Views/Map/MapPage.cs
@@ -9,28 +9,19 @@ namespace Phoenix.Views.Map
 	{
 		public MapPage()
 		{
-
-			var assembly = typeof(MapPage).GetTypeInfo().Assembly;
-			Stream stream = assembly.GetManifestResourceStream("Phoenix.Views.Map.CristoRei.svg");
-
-			string text;
-			using (var reader = new System.IO.StreamReader (stream)) {
-				text = reader.ReadToEnd ();
-			}
-
-
-			var htmlSource = new HtmlWebViewSource ();
-			htmlSource.Html = text;
-
-
+	
 			var browser = new WebView {
-				Source = htmlSource,
-				VerticalOptions = LayoutOptions.FillAndExpand
+				Source = "http://ariutta.github.io/svg-pan-zoom/demo/custom-controls.html",
+				VerticalOptions = LayoutOptions.FillAndExpand,
+				WidthRequest = 240
 			};
 
 			var buttonWhereAmI = new Button { Text = "Onde eu estou?" };
 
 			var familiarName = new Entry { Placeholder = "Nome ou CPF" };
+
+
+
 
 			Content = new StackLayout {
 				Children = {
