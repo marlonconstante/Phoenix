c9e52bc2f65521e87978fe1ff17522d79f3f9b23
Marlon Santos Constante <marlon.constante@gmail.com>
2014-09-03 15:13:18 -0300

    Alterando cores da barra de navegação

--- Phoenix/App.cs
+++ Phoenix/App.cs
@@ -6,17 +6,19 @@ namespace Phoenix
 {
 	public class App
 	{
+		/// <summary>
+		/// Gets the main page.
+		/// </summary>
+		/// <returns>The main page.</returns>
 		public static Page GetMainPage()
 		{	
 			var mainNav = new NavigationPage(new EnterpriseSelectionPage())
 			{
-				BarBackgroundColor = Color.White,
-				BarTextColor = Color.FromRgb(56, 56, 56)
+				BarBackgroundColor = Color.FromRgb(248, 248, 248),
+				BarTextColor = Color.FromRgb(86, 86, 86)
 			};
 					
 			return mainNav;
 		}
-			
 	}
 }
-
